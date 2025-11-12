13a5b4f671050eedc32f5b5377b569fcfdffdd2a
KriskerOG <jeremychi617@gmail.com>
2025-11-12 18:42:53 +0800

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -1,5 +1,5 @@
 import random
-with open(‘participants.txt’, ‘r’) as f:
+with open('participants.txt', 'r') as f:
     participants = f.readlines()
   winner = random.choice(participants)
 print(“恭喜”, winner.strip(), “中奖了！”)
